13cfc5987ef3f9b2042753eb040c078241a2a070
yurilima5 <yurilimarosa12@gmail.com>
2024-11-26 20:36:55 -0300

    finalização footer, correção de fontes e finalização do componente inicio

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,10 +8,17 @@ export default function Footer() {
     return (
       <footer className={s.footer}>
 
-       <section className={s.redesSociais}>
-        <nav>
+       <section className={s.sessaoFooter}>
+        <h2 className={s.telefone}>4002-8922</h2>
+        <nav className={s.navFooter}>
+          <a href=''><img className={s.icones} src={facebook} alt="icone do facebook" /></a>
+          <a href=''><img className={s.icones} src={twitter} alt="icone do twitter" /></a>
+          <a href=''><img className={s.icones} src={youtube} alt="icone do youtube" /></a>
+          <a href=''><img className={s.icones} src={linkedin} alt="icone do linkedin" /></a>
+          <a href=''><img className={s.icones} src={instagram} alt="icone do instagram" /></a>
         </nav>
       </section>
+      <h2 className={s.aviso}>Layout desenvolvido pela Vai Na Web para fins educativos - 2024  </h2>
       </footer>
     );
   }
--- src/pages/queroDoar/queroDoar.jsx
+++ src/pages/queroDoar/queroDoar.jsx
@@ -1,5 +1,14 @@
+import s from "../queroDoar/doar.module.scss"
 export default function QueroDoar() {
     return(
-        <h2>sou a pagina de doação</h2>
+        <main>
+            <h1>Por favor, preencha o formulário com suas informações e as informações do Livro</h1>
+            <section>
+                <input type="text" placeholder="aqui" />
+                <input type="text" placeholder="" />
+                <input type="text" placeholder=""/>
+                <input type="text" placeholder=""/>
+            </section>
+        </main>
     )
 }
