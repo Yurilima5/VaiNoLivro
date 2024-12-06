d6e1fad067aadd2a4686e7a32fe86e9a599a668f
yurilima5 <yurilimarosa12@gmail.com>
2024-12-06 10:39:17 -0300

    atualização @media, responsivo

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -7,38 +7,38 @@ export default function Inicio(){
 return(
     <main>
             <section className={s.primeiraSessao}>
-                <p className={s.p1}>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</p>
+                <p className={s.texto1}>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</p>
             </section>
 
     <section className={s.segundaSessao}>
-            <h1 className={s.h1}
+            <h1 className={s.porque}
             >Por que devo doar?</h1>            
         
          <section className={s.sessaoContainer}>
          {/* seguimento dos conteiners, forma que encontrei de deixar mais semantico e organizado */}
             <section className={s.container}>
-             <img 
+             <img className={s.imagens}
              src={comunidade}
              alt="uma imagem representando uma rede/uniao de pessoas" />
              <p className={s.containerP}> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
             </section>
 
             <section className={s.container}>
-             <img 
+             <img className={s.imagens} 
              src={lendo}
              alt="imagem representando alguém lendo um livro" />
              <p className={s.containerP}>Estimula o hábito da leitura e o aprendizado contínuo.</p>
             </section>
 
             <section className={s.container}>
-             <img 
+             <img className={s.imagens} 
              src={forca}
              alt="desenho ilustrando a força de pessoas unidas" />
              <p className={s.containerP}>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
             </section>
 
             <section className={s.container}>
-             <img 
+             <img className={s.imagens} 
              src={balanca}
              alt="imagem de uma balança, representando o contexto do texto abaixo" />
              <p className={s.containerP}>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
